865b8deda837ab5a59e78a832f540e4545644d39
KONDI SRINIVASU <srinivasu.kondi@gspann.com>
2024-10-22 13:08:00 +0530

    BreadCrumb 2.1

--- blocks/bread-crumb/bread-crumb.js
+++
@@ -1,8 +0,0 @@
-import { decorateIcons, getMetadata } from '../../scripts/aem.js';
-import {
-  div, nav, ol, li, a, span,
-} from '../../scripts/dom-builder.js';
-
-export default function decorate(block) {
-
-}
