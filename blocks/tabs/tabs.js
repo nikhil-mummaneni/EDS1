30731b9e3542bc0b4b8bdc689e4d98aa8d36d105
sugeetsharma <sugeet.sharma@gspann.com>
2024-10-24 05:45:29 +0530

    alert message

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,5 +1,8 @@
+import { decorateIcons } from '../../scripts/aem.js';
+import { div, span } from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
   const tw = document.createElement('div');
-  alert("test 1");
+  alert('test 1');
   block.append(tw);
 }
