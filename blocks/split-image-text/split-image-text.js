484e0c351ce89b0ad28571ac7ad2ea49ae00f7c1
subrahmanyam <subrahmanyam.y@gspann.com>
2024-10-25 11:40:43 +0530

    div issues fix changes

--- blocks/split-image-text/split-image-text.js
+++ blocks/split-image-text/split-image-text.js
@@ -23,11 +23,9 @@ export default function decorate(block) {
   imageTag.src = image.src;
   imageDiv2.appendChild(imageTag);
 
-  const headingContainer = block.getElementsByTagName('div')[3];
-  const heading = headingContainer.querySelector('h3 strong');
-
-  const linkContainer = block.getElementsByTagName('div')[4];
-  const link = linkContainer.querySelector('h3 strong');
+  const headingContainer = block.getElementsByTagName('div')[4];
+  const heading = headingContainer.querySelector('h3');
+  const link = headingContainer.querySelector('a);
 
   const contentDiv = div({ class: 'ssplit-component-content tw-w-full md:tw-w-6/12 tw-pt-24 md:tw-pt-0 md:tw-pl-[4.1666666666667%] lg:tw-px-[4.1666666666667%] tw-flex tw-flex-col tw-justify-center' });
   parentDiv.appendChild(contentDiv);
