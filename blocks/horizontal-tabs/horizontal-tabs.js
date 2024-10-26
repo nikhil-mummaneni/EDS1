4179d4f3db224cd62f3c967faf96d596aa01591a
Kotireddy <koti.naru@gspann.com>
2024-10-26 17:29:04 +0530

    testing

---
+++ blocks/horizontal-tabs/horizontal-tabs.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-console
+/* import * as domB from '../../scripts/dom-builder.js'; */
+
+const tabComponentSection = document.createElement('section');
+tabComponentSection.setAttribute('class', 'tw-pt-32 md:tw-pt-48 tw-pb-32 md:tw-pb-48');
+
+export default async function decorate(block) {
+  block.textContent = '';
+  block.append('Hello everyone');
+}
