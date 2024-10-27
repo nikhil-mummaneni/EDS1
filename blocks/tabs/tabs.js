98e7f648da55e0d6c5372be9355764f88926f6fb
koti-naru <koti.naru@gspann.com>
2024-10-27 19:12:17 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,10 +1,1 @@
-// eslint-disable-next-line no-console
-/* import * as domB from '../../scripts/dom-builder.js'; */
 
-const tabComponentSection = document.createElement('section');
-tabComponentSection.setAttribute('class', 'tw-pt-32 md:tw-pt-48 tw-pb-32 md:tw-pb-48');
-
-export default async function decorate(block) {
-  block.textContent = '';
-  block.append('Hello everyone');
-}
