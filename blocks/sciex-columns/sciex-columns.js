90dd76420afb169349196621c0b451a46182f31c
KONDI SRINIVASU <srinivasu.kondi@gspann.com>
2024-11-01 13:09:29 +0530

    Intal commit1

--- blocks/sciex-columns/sciex-columns.js
+++ blocks/sciex-columns/sciex-columns.js
@@ -1,28 +1,21 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-console.log("inside");
-  // Create a <ul> element to hold the card items
+  /* change to ul, li */
   const ul = document.createElement('ul');
-
-  // Iterate through the block children to create <li> elements
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
-
     [...li.children].forEach((div) => {
-      // Apply styles and classes to images
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        div.className = 'cards-card-image tw-mb-16 md:tw-mb-20';
-      } else {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image tw-mb-16 md:tw-mb-20';
+      else {
         div.className = 'cards-card-body';
 
         const button = div.querySelector('button');
         if (button) {
-          button.remove(); // Remove existing button if present
+          button.remove();
         }
 
-        // Create a call-to-action link
         const cta = document.createElement('a');
         cta.className = 'cta';
         cta.href = '#';
@@ -30,16 +23,9 @@ console.log("inside");
         div.append(cta);
       }
     });
-
-    // Append the <li> to the <ul>
     ul.append(li);
   });
-
-  // Optimize images
-  ul.querySelectorAll('picture > img').forEach((img) =>
-    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-
-  // Clear the block and append the new list
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
   block.classList.add("tw");
