e64bd6002417e8f33b41b2bda09e17442ebaae75
subrahmanyam <subrahmanyam.y@gspann.com>
2024-11-07 16:16:14 +0530

    Media grid changes

--- blocks/media-grid/media-grid.js
+++ blocks/media-grid/media-grid.js
@@ -2,7 +2,6 @@ import {
   div
 } from '../../scripts/dom-builder.js';
 
-// breadcrumb functionality implementation
 export default function decorate(block) {
   const sectionDiv = document.createElement('section');
   sectionDiv.className = 'global-pt-32 md:global-pt-48 global-pb-32 md:global-pb-48';
@@ -22,16 +21,9 @@ export default function decorate(block) {
   const contentContainer = container.querySelector('div');
   const image = contentContainer.querySelector('picture > img');
   const imageTag = document.createElement('img');
-  imageTag.className = 'tw-transition-all tw-duration-500 tw-inset-0 tw-top-0 tw-left-0 tw-w-full tw-h-full tw-object-cover hover:tw-scale-[1.05] motion-reduce:hover:tw-transform-none';
+  imageTag.className = 'global-transition-all global-duration-500 global-inset-0 global-top-0 global-left-0 global-w-full global-h-full global-object-cover hover:global-scale-[1.05] motion-reduce:hover:global-transform-none';
   imageTag.src = image.src;
   imageDiv1.appendChild(imageTag);
-
-  const imageDiv = div({ class: 'global-w-full md:global-w-1/2 global-flex global-flex-col media-grid-item  md:global-pt-0 false' });
-  parentDiv.appendChild(imageDiv);
-  const anchorTag = document.createElement('a');
-  const imageDiv1 = div({ class: 'global-w-full global-relative global-overflow-hidden global-aspect-[8/5]' });
-  imageDiv.appendChild(anchorTag);
-  anchorTag.appendChild(imageDiv1);
 
   block.textContent = '';
   block.append(sectionDiv);
