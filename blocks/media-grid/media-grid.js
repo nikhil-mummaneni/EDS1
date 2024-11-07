e6fd9449b51c89b188b5d62e488b4a095dedd03e
subrahmanyam <subrahmanyam.y@gspann.com>
2024-11-07 21:25:21 +0530

    Media grid changes

--- blocks/media-grid/media-grid.js
+++ blocks/media-grid/media-grid.js
@@ -11,29 +11,29 @@ export default function decorate(block) {
   parentDiv.appendChild(childDiv);
 
   const childrenDivs = block.children;
-  Array.from(childrenDivs).forEach((childDiv, index) => {
+  Array.from(childrenDivs).forEach((child, index) => {
     const imageDiv = div({ class: 'global-w-full md:global-w-1/2 global-flex global-flex-col media-grid-item  md:global-pt-0 false' });
-    childDiv.appendChild(imageDiv);
     const anchorTag = document.createElement('a');
     const imageDiv1 = div({ class: 'global-w-full global-relative global-overflow-hidden global-aspect-[8/5]' });
-    imageDiv.appendChild(anchorTag);
-    anchorTag.appendChild(imageDiv1);
 
-    if (!childDiv) return; // Check if childDiv exists
-    const container = childDiv.querySelector('div');
+    if (!child) return; // Check if childDiv exists
+    const container = child.querySelector('div');
     const image = container.querySelector('picture > img');
     const imageTag = document.createElement('img');
     imageTag.className = 'global-transition-all global-duration-500 global-inset-0 global-top-0 global-left-0 global-w-full global-h-full global-object-cover hover:global-scale-[1.05] motion-reduce:hover:global-transform-none';
     imageTag.src = image.src;
     imageDiv1.appendChild(imageTag);
-    if(container.getElementsByTagName('div').length > 1) {
-      const link = container.getElementsByTagName('div')[1].querySelector('p > a');
+    if(child.getElementsByTagName('div').length > 1) {
+      const link = child.getElementsByTagName('div')[1].querySelector('p > a');
       anchorTag.href = link.href;
       anchorTag.target = '_blank';
       }
-    });
+    anchorTag.appendChild(imageDiv1);
+    imageDiv.appendChild(anchorTag);
+    childDiv.appendChild(imageDiv);
+  });
 
-    // Styling adjustments for parent element
+  // Styling adjustments for parent element
   block.parentElement.style.maxWidth = '100%';
   block.parentElement.style.padding = '0';
 
@@ -43,4 +43,4 @@ export default function decorate(block) {
   });
   //block.textContent = '';
   block.append(sectionDiv);
-  }
+}
