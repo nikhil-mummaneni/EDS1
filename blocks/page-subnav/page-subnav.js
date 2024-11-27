1344fc06fb3f0cf0602eba1bca2033aeccba8697
gspann-content <gspann.content@gmail.com>
2024-11-27 11:34:26 +0530

    add page-subnav folder

---
+++ blocks/page-subnav/page-subnav.js
@@ -0,0 +1,12 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(blockElement) {
+
+    [...blockElement.children].forEach((rowElement) => {
+ 
+        [...row.children].forEach((col, index) => {
+        }
+    }
+         
+}
