10868462d09bf7fd767ad09321c2bf5fae9f7b2c
Santosh-eng-gspann <santosh.bhadrashette@gspann.com>
2024-12-30 19:50:35 +0530

    hero banner reverted

--- blocks/hero-banner/hero-banner.js
+++ blocks/hero-banner/hero-banner.js
@@ -44,7 +44,8 @@ export default function decorate(block) {
   });
   contentWrapper.appendChild(flexContainer);
 
-  
+  const textContainer = div({ class: 'global-w-full' });
+  flexContainer.appendChild(textContainer);
 
   // Add heading and paragraph
   const heading = block.querySelector('h1');
