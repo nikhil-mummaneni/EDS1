fc08f5346f129507f587e68b5ee72565e3152ef7
Santosh-eng-gspann <santosh.bhadrashette@gspann.com>
2024-12-31 11:39:25 +0530

    Child

--- blocks/contact-support/contact-support.js
+++ blocks/contact-support/contact-support.js
@@ -1,16 +1,20 @@
-export default function decorate(block) {
+/*export default function decorate(block) {
   // add main class to parent block
   block.classList.add('page-title-container-santosh');
 
   // select the child and specific classes
  
 
-  if (textDiv) {
-    textDiv.classList.add('page-title-text');
-    const heading = document.createElement('h1');
-    heading.textContent = textDiv.textContent;
-    textDiv.textContent = '';
-    // clear the existing text
-    textDiv.appendChild(heading);
+}
+*/
+
+export default function decorate(block) {
+  // Add main class to the parent block
+  block.classList.add('page-title-container-santosh');
+
+  // Add the class to the first child of the block element
+  const firstChild = block.firstElementChild; // Get the first child element
+  if (firstChild) {
+    firstChild.classList.add('page-title-container-santosh'); // Add the class to the first child
   }
 }
